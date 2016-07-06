776cc3b04d707df2162534af7336618ef191e6be
Francesco <francesco.benetello921@gmail.com>
2016-07-07 00:13:53 +0200

    Corrette funzioni per il corretto funzionamento del login, ricerca e registrazione

--- Sito/functions.php
+++ Sito/functions.php
@@ -26,7 +26,7 @@ error_reporting(E_ALL & ~E_NOTICE);
             $stat->execute();
             foreach($stat as $record) 
             {
-                echo "<font face=arial> $record[login] $record[nome] $record[cognome] $record[indirizzo] $record[password] </font><br>";     
+                echo "<font face=arial> username: $record[login] nome: $record[nome] cognome: $record[cognome] indirizzo: $record[indirizzo] password: $record[password] </font><br>";     
             }
             
             return $stat;
@@ -45,23 +45,33 @@ error_reporting(E_ALL & ~E_NOTICE);
 
     function cerca_pizze($dbconn, $parola) {
         $parola_con_wildcard = '%'.$parola.'%';
-        /*$stat = $dbconn->prepare('select nome,prezzo from pizze where nome ilike ?');
-        $stat->execute(array($parola_con_wildcard));*/
+        
+        if(!(empty($parola)))
+        {
+
         $stat=$dbconn->prepare('select p.nome,p.prezzo, array_to_string(array_agg(m.nomeingrediente),?) as ingredienti from disponibilitaingredienti di,pizze p ,magazzino m where p.idpizza=di.idpizza and m.idingrediente=di.idingrediente and nome ilike ? group by p.nome,p.prezzo order by p.prezzo');
             
             $stat->execute(array(',',$parola_con_wildcard));
         return $stat;
+        }
+        
     }
+
     
-    function inserisci_utente($dbconn)
+    
+    /*function inserisci_utente($dbconn)
     {
         $stat=$dbconn->prepare('insert into utenti (login,nome,cognome,indirizzo,password,numerotelefono) values(?,?,?,?,?,?)');
-        $stat->execute(array($_POST['login'],$_POST['nome'],$_POST['cognome'],$_POST['indirizzo'],$_POST['password'],$_POST['telefono']));
-        return $stat;         
+        $stat->execute(array($_POST['login'],$_POST['nome'],$_POST['cognome'],$_POST['indirizzo'],md5($_POST['password']),$_POST['telefono']));
+        return $stat; 
+        
+        create or replace function crea_utente(log text,name text,surname text,address text,pass text,tel text) returns void as $$
+  insert into utenti (login,nome,cognome,indirizzo, password,numerotelefono) values (log,name,surname,address,md5(pass),tel);
+$$ language sql;       
     
       
     
-    }
+    }*/
     
         function restituisci_ingredienti($dbconn)
     {
@@ -72,6 +82,29 @@ error_reporting(E_ALL & ~E_NOTICE);
     
     }
     
+     function prezzo_totale($dbconn)
+    {
+        $stat=$dbconn->prepare('select * from magazzino');
+        $stat->execute();
+        return $stat;         
+         
+    
+    }
+    
+    
+        function dati_utenti_registrati($dbconn)
+    {
+            $stat=$dbconn->prepare('select nome,cognome,indirizzo,login,numerotelefono from utenti where login=?');
+            $stat->execute(array($_SESSION['login']));
+            foreach($stat as $record) 
+            {
+                echo "<font face=arial> <br>username: $record[login] <br> nome: $record[nome]<br> cognome: $record[cognome]<br> indirizzo: $record[indirizzo] <br> numero di telefono: $record[numerotelefono]</font><br>";     
+            }
+            
+            return $stat;
+    }
+    
+
     
 
 ?>
