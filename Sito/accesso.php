f8ab3dffb66645d2d14cab0ddb35cb234f03a80e
Francesco <francesco.benetello921@gmail.com>
2016-07-07 00:12:30 +0200

    Corretto login

--- Sito/accesso.php
+++ Sito/accesso.php
@@ -1,73 +1,42 @@
-<!DOCTYPE html> 
-<html>
-    <head>
-        <title> Progetto Basi Di Dati </title>
-        <meta http- equiv="content-type" content="text/html" charset="UTF-8" lang="it-IT">
-    </head>
-
-    
-    <body style="background-color:skyblue;">
-    
-        <h1 style="text-align:center;font-family:arial;"> Under construction (login effettuato con successo)</h1>
-        
-        <a href="index.php"><Button style="text-align:center;font-family:arial;">Ritorna alla pagina principale</button></a>
-    
+   
 <?php
-require "functions.php";
+  require "functions.php";
 
+    if(!(isset($_POST['Accedi']))) {
 
-if(!(isset($_POST['Accedi'])))
-{
+        if (empty($_POST['login']) || empty($_POST['password'])) {
+            header('Location:index.php?errore=campivuotilogin');
+        }
 
-    if (empty($_POST['login']) || empty($_POST['password'])) {
-        header('Location:index.php?errore=campivuotilogin');
-  
-    }
-
-
-    else
-    {
-        if(is_numeric($_POST['telefono']))
-        {
-            try
-            {
+        else {
+            try {
                 $dbconn = db_connection();
-                $statement = $dbconn->prepare('select credenziali_valide(?, ?)');
+                $statement = $dbconn->prepare('select dati_validi(?, ?)');
                 $statement->execute(array($_POST['login'],$_POST['password']));
                 $rec = $statement->fetch();
-                  if ($rec[0]==1) 
-                    {
-                        header('Location:accesso.php');
+                if ($rec[0]==1) {
+                    $dbconn = db_connection();
+                    $stat = $dbconn->prepare('select is_admin(?)');
+                    $stat->execute(array($_POST['login']));
+                    $res = $stat->fetch();  
+                    if($res[0]==true) {
+                        header('Location:admin.php');
                         session_start();                            
-                        $_SESSION['login'] = $_POST['login']; 
-                    } 
-                  else 
-                  {
-                    header('Location:index.php?errore=credenzialiinvalide');
-                  }   
-    
+                        $_SESSION['login'] = $_POST['login'];
+                    }
+                    else {
+                        header('Location:user.php');
+                        session_start();                            
+                        $_SESSION['login'] = $_POST['login'];
+                    }                       
+                } 
+                else {
+                header('Location:index.php?errore=credenzialiinvalide');
+                }     
             }
-            catch (PDOException $e) { echo $e->getMessage(); }
-         }  
-         else
-         {
-            echo "il numero di telefono deve essere formato solo da numeri compresi tra 0 e 9 (ma non è così)";
-         
-         }   
+        catch (PDOException $e) { echo $e->getMessage(); }
+        }
+
     }
 
-}
-
-
-
-
-
-echo "<p style=font-family:arial;><br>I tuoi dati di accesso sono:<br> login=".$_POST['login']."<br> password=".$_POST['password']."</p>";
-
 ?>
-
-
-    
-    </body>
-    </head>
-</html>
