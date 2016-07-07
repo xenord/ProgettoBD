8b331fedc4ad029997ce6ffd9576184075c280f1
Francesco <francesco.benetello921@gmail.com>
2016-07-07 17:56:37 +0200

    fix link

--- Sito/admin.php
+++ Sito/admin.php
@@ -20,7 +20,7 @@
   <ul>
     <li> <a href="lista_ordini.php"><Button style="text-align:center;">Elencare tutti gli ordini effettuati</button></a> </li><br>
     <li> <a href="crea_ordine.php"><Button style="text-align:center;">Creare un nuovo ordine</button></a> </li><br>
-    <li> <a href="crea_ordini.php"><Button style="text-align:center;">Cancellare un' ordine</button></a> </li><br>
+    <li> <a href="crea_ordine.php"><Button style="text-align:center;">Cancellare un' ordine</button></a> </li><br>
         <li> <a href="pizze.php"><Button style="text-align:center;">Visualizzare le pizze(potrebbe non servire)</button></a> </li><br>
     <li> <a href="personale.php">Vedere i tuoi dati personali(potrebbe non servire)</a> </li><br>
     <li> <a href="logout.php"><Button style="text-align:center;">Effettuare il logout</button></a></li><br>
