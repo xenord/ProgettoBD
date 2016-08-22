ab710bb7f7cd66944ea0e4247ff70ce996efb4e8
Francesco <francesco.benetello921@gmail.com>
2016-08-22 12:00:52 +0200

    FIX: function

--- Sito/functions.php
+++ Sito/functions.php
@@ -71,8 +71,7 @@ $$ language sql;
     
     }*/
     
-        function restituisci_ingredienti($dbconn)
-    {
+    function restituisci_ingredienti($dbconn) {
         $stat=$dbconn->prepare('select * from magazzino');
         $stat->execute();
         return $stat;         
@@ -103,13 +102,14 @@ $$ language sql;
     }
     
     function stampa_ordini($dbconn) {
-        $stat=$dbconn->prepare('select * from ordini');
+        $stat=$dbconn->prepare('select o.idordine, o.login, o.giornoconsegna, o.oraconsegna, o.indirizzoconsegna, p.nome, pc.numeropizze from ordini o, pizzecontenute pc, pizze p where pc.idordine = o.idordine and p.idpizza = pc.idpizza');
         $stat->execute();        
         return $stat;
     }
 
+
     function stampa_ordini_per_utente($dbconn) {
-        $stat=$dbconn->prepare('select * from ordini where login = ?');
+        $stat=$dbconn->prepare('select o.idordine, o.login, o.giornoconsegna, o.oraconsegna, o.indirizzoconsegna, p.nome, pc.numeropizze from ordini o, pizzecontenute pc, pizze p where pc.idordine = o.idordine and p.idpizza = pc.idpizza and login = ?');
         $stat->execute(array($_SESSION['login']));
         return $stat;
     }
