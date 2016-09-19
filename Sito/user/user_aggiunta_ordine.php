6cca48c837c42d78785294eabdd9fbc60c2bdf3d
Francesco <francesco.benetello921@gmail.com>
2016-09-19 13:11:57 +0200

    Update user_aggiunta_ordine.php

--- Sito/user/user_aggiunta_ordine.php
+++ Sito/user/user_aggiunta_ordine.php
@@ -1,38 +1,41 @@
 <?php  
     require "../functions.php"; 
     verifica_accesso();
-    if (empty($_POST['indirizzo'])) {
+    if (!isset($_POST['indirizzo'])) {
         header('Location:user_crea_ordine.php?errore=campivuoti');
     }
-    $current_user = $_SESSION['login'];
-
-    $year = $_POST['year'];
-    $month = $_POST['months'];
-    $day = $_POST['days'];
-    $dateformat = $year."-".$month."-".$day;
-
-    $minute=$_POST['minutes'];
-    $hour=$_POST['hours'];
-    $timeformat=$hour.":".$minute.":"."00";
-
-    try {
-        $dbconn = db_connection();
-        $check = $dbconn->prepare('select count(*) from ordini where login = ? and giornoconsegna = ?');
-        $check->execute(array($current_user,$dateformat));
-        foreach ($check as $check_o) {
-            if ($check_o[0] == 1) {
-                echo "Ordine esistente!";
-                echo "<br></br>";
-                echo "Cambiare data oppure cancella l'ordine e rifai";
-                header( "refresh:2;url=user.php" );
+    else
+    {
+        $current_user = $_SESSION['login'];
+    
+        $year = $_POST['year'];
+        $month = $_POST['months'];
+        $day = $_POST['days'];
+        $dateformat = $year."-".$month."-".$day;
+    
+        $minute=$_POST['minutes'];
+        $hour=$_POST['hours'];
+        $timeformat=$hour.":".$minute.":"."00";
+    
+        try {
+            $dbconn = db_connection();
+            $check = $dbconn->prepare('select count(*) from ordini where login = ? and giornoconsegna = ?');
+            $check->execute(array($current_user,$dateformat));
+            foreach ($check as $check_o) {
+                if ($check_o[0] == 1) {
+                    echo "Ordine esistente!";
+                    echo "<br></br>";
+                    echo "Cambiare data oppure cancella l'ordine e rifai";
+                    header( "refresh:2;url=user.php" );
+                }
+                else {
+                    $statement = $dbconn->prepare('select crea_ordine(?,?,?,?)');
+                    $statement->execute(array($current_user,$dateformat,$timeformat,$_POST['indirizzo']));
+                    header('Location: user_aggiunta_pizze.php');
+                }
             }
-            else {
-                $statement = $dbconn->prepare('select crea_ordine(?,?,?,?)');
-                $statement->execute(array($current_user,$dateformat,$timeformat,$_POST['indirizzo']));
-                header('Location: user_aggiunta_pizze.php');
-            }
-        }
-    } catch (PDOException $e) { echo $e->getMessage(); }
+        } catch (PDOException $e) { echo $e->getMessage(); }
+    }
 
 ?>
 
